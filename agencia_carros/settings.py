724f1f7f0e0e735c8c74a7ee67adf84db6449a69
Pablo Gómez <pablogomez232002@gmail.com>
2024-11-28 02:40:56 -0500

    Actualiza formulario de vehículo y corrige conflictos

--- agencia_carros/settings.py
+++ agencia_carros/settings.py
@@ -1,25 +1,17 @@
 import os
 from pathlib import Path
-<<<<<<< HEAD
 import dj_database_url
-=======
->>>>>>> fc8ba0a41a8635567d5705060d161bf95028d91e
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'your-secret-key'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -28,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    # Add your apps here
     'concesionario',
 ]
 
@@ -48,13 +39,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-<<<<<<< HEAD
             os.path.join(BASE_DIR, 'static/templates'),
             os.path.join(BASE_DIR, 'concesionario/templates'),
-=======
-            os.path.join(BASE_DIR, 'static', 'templates'),
-            os.path.join(BASE_DIR, 'concesionario', 'templates')
->>>>>>> fc8ba0a41a8635567d5705060d161bf95028d91e
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -70,17 +56,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'agencia_carros.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-<<<<<<< HEAD
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',     # El nombre de la base de datos de tu configuración
-        'USER': 'postgres',       # Tu usuario
-        'PASSWORD': 'password',   # Tu contraseña
-        'HOST': 'localhost',      # Cambiado de 'db' a 'localhost' para desarrollo local
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
@@ -90,20 +72,6 @@ database_url = os.getenv('DATABASE_URL')
 if database_url:
     DATABASES['default'] = dj_database_url.parse(database_url)
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-=======
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
->>>>>>> fc8ba0a41a8635567d5705060d161bf95028d91e
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -119,26 +87,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-<<<<<<< HEAD
-
-=======
->>>>>>> fc8ba0a41a8635567d5705060d161bf95028d91e
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-<<<<<<< HEAD
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
@@ -148,24 +102,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-=======
-# Configuración de archivos estáticos
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Configuración de archivos multimedia
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
->>>>>>> fc8ba0a41a8635567d5705060d161bf95028d91e
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
